07cf320b6c706d6d6fdab8e69f87b4756f963a61
Seal <info@sealsecurity.io>
2026-09-19 09:02:01 +0000

    CVE-2016-10707

--- test/unit/attributes.js
+++ test/unit/attributes.js
@@ -477,7 +477,9 @@ test( "attr(String, Object)", function() {
 test( "attr - extending the boolean attrHandle", function() {
 	expect( 1 );
 	var called = false,
-		_handle = jQuery.expr.attrHandle.checked || $.noop;
+		origAttrHandleHadChecked = "checked" in jQuery.expr.attrHandle,
+		origAttrHandleChecked = jQuery.expr.attrHandle.checked,
+		_handle = origAttrHandleChecked || $.noop;
 	jQuery.expr.attrHandle.checked = function() {
 		called = true;
 		_handle.apply( this, arguments );
@@ -486,6 +488,12 @@ test( "attr - extending the boolean attrHandle", function() {
 	called = false;
 	jQuery( "input" ).attr( "checked" );
 	ok( called, "The boolean attrHandle does not drop custom attrHandles" );
+
+	if ( origAttrHandleHadChecked ) {
+		jQuery.expr.attrHandle.checked = origAttrHandleChecked;
+	} else {
+		delete jQuery.expr.attrHandle.checked;
+	}
 });
 
 test( "attr(String, Object) - Loaded via XML document", function() {
@@ -1475,4 +1483,53 @@ test( "Insignificant white space returned for $(option).val() (#14858)", functio
 
 	val = jQuery( "<option>  test  </option>" ).val();
 	equal( val.length, 4, "insignificant white-space returned for value" );
+});
+
+test( "non-lowercase boolean attribute getters should not crash", function() {
+	expect( 3 );
+
+	var elem = jQuery( "<input checked required autofocus type='checkbox'>" );
+
+	jQuery.each({
+		checked: "Checked",
+		required: "requiRed",
+		autofocus: "AUTOFOCUS"
+	}, function( lowercased, original ) {
+		try {
+			strictEqual( elem.attr( original ), lowercased,
+				"The '" + this + "' attribute getter should return the lowercased name" );
+		} catch ( e ) {
+			ok( false, "The '" + this + "' attribute getter threw" );
+		}
+	});
+});
+
+test( "non-lowercase boolean attribute lookups should not recurse infinitely", function() {
+	expect( 4 );
+
+	var elem = jQuery( "<input checked required autofocus type='checkbox'>" )[ 0 ];
+
+	// jQuery.attr lowercases the name before it reaches the boolean attrHandle,
+	// so exercise the vulnerable getter the way Sizzle does: with the raw name.
+	jQuery.each({
+		checked: "Checked",
+		required: "requiRed",
+		autofocus: "AUTOFOCUS"
+	}, function( lowercased, original ) {
+		try {
+			strictEqual( jQuery.find.attr( elem, original ), lowercased,
+				"jQuery.find.attr with the '" + this + "' name returns the lowercased name" );
+		} catch ( e ) {
+			ok( false, "jQuery.find.attr with the '" + this + "' name threw" );
+		}
+	});
+
+	try {
+		strictEqual(
+			jQuery( "#qunit-fixture input" ).filter("[Checked]").length,
+			jQuery( "#qunit-fixture input" ).filter("[checked]").length,
+			"A non-lowercase boolean attribute selector matches the lowercase one" );
+	} catch ( e ) {
+		ok( false, "Filtering by a non-lowercase boolean attribute selector threw" );
+	}
 });
